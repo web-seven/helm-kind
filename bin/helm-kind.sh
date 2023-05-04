7f1cfca93a4b7897849529b4fcb31f611a8922c0
Evgheni <web7evgheni@gmail.com>
2023-05-04 14:47:01 +0300

    added metrics server to kind cluster start

--- bin/helm-kind.sh
+++ bin/helm-kind.sh
@@ -9,4 +9,5 @@ if [ -e gcp.json ]
 then
     kubectl create secret docker-registry gcr-json-key --docker-server=eu.gcr.io --docker-username=_json_key --docker-password="$(cat ./gcp.json)" --docker-email=any@valid.email;
 fi
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.6.3/components.yaml
 
